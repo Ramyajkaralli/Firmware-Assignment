0eb97bec5e71a288321b6cf01edf8512b4f1ae02
Ramyajkaralli <101457935+Ramyajkaralli@users.noreply.github.com>
2023-11-29 12:28:11 +0530

    Firmware_Assignment.ino

--- Firmware_Assignment.ino
+++ Firmware_Assignment.ino
@@ -1,39 +1,46 @@
 #include<stdint.h>
 
-long count = 0;                       //defining a count variabe for counting
+#define Threshold_temperatue 30                       //defining threshold temperature value as 30°C 
+
+long count = 0;                                       //defining a count variabe for counting
 
 void setup()
 {
-  pinMode(A0, INPUT);                 //Set A0 pin as INPUT
-  pinMode(LED_BUILTIN, OUTPUT);       //Set Built-in LED pin as OUTPUT
+  pinMode(A0, INPUT);                                 //Set A0 pin as INPUT
+  pinMode(LED_BUILTIN, OUTPUT);                       //Set Built-in LED pin as OUTPUT
 }
 
 void loop()
 {
-  int x = analogRead(A0);             //Read the temperature data from analog pin A0
-
-  if (x < 30)                         //Check the data < threshold value(30 degree celcius)
+  int data = analogRead(A0);                          //Read the temperature data from analog pin A0
+  float voltage = (data / 1023.0) * 5.0;              // Convert analog value to voltage (0 to 5V)
+  float temperatureCelsius = (voltage - 0.5) * 100.0; // Convert voltage to degrees Celsius using LM35 formula
+  
+  //////////////Conditional loop/////////////////
+  if (temperatureCelsius < Threshold_temperature)     //Check the temperatureCelsius < threshold value
   {
-   /////////// BLINK LED with a delay of 250ms if temperature < 30 //////////
-   count++;                           //Increment Count by 1
-   if(count == 100000){               //Check if count value reaches to 100000
-     digitalWrite(LED_BUILTIN,HIGH);  //Turn LED ON by writing HIGH
-   }
-   else if(count == 200000){          //Check if count value reaches to 200000
-     count = 0;                       //Reset Count to 0
-     digitalWrite(LED_BUILTIN,LOW);   //Turn OFF LED by writing LOW
-   }
+    /////////// BLINK LED with a delay of 250ms if temperature < 30 //////////
+    digitalWrite(LED_BUILTIN, HIGH);                  // Turn LED ON by writing HIGH (Sending 5v to pin 13)
+    delay_ms(250);                                    //Delay of 500 milliseconds
+    digitalWrite(LED_BUILTIN, LOW);                   //Turn OFF LED by writing LOW
+    delay_ms(250);                                    //Delay of 500 milliseconds
   }
   else 
   {
    /////////// BLINK LED with a delay of 500ms if temperature > 30 //////////
-   count++;                           //Increment Count by 1
-   if(count == 200000){               //Check if count value reaches to 200000
-     digitalWrite(LED_BUILTIN,HIGH);  //Turn LED ON by writing HIGH
-   }
-   else if(count == 400000){           //Check if count value reaches to 400000
-     count = 0;                        //Reset Count to 0
-     digitalWrite(LED_BUILTIN,LOW);    //Turn OFF LED by writing LOW
+   digitalWrite(LED_BUILTIN, HIGH);                   // Turn LED ON by writing HIGH (Sending 5v to pin 13)
+    delay_ms(500);                                    //Delay of 500 milliseconds
+    digitalWrite(LED_BUILTIN, LOW);                   //Turn OFF LED by writing LOW
+    delay_ms(500);   //Delay of 500 milliseconds
    }
   }
+}
+
+/////////////////Delay Function in milliseconds////////////////
+
+void delay_ms(unsigned long ms)                      //Function for delay in milliseconds 
+{
+    for (unsigned long i = 0; i < ms; i++) {
+        for (volatile uint32_t j = 0; j < 400; j++); // loop to generate 1 millisecond delay
+    }
 }
